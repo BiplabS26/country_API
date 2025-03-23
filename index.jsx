728c3933e3053bad29c95abca7635a53c4a5840d
BiplabS26 <sbiplab26@gmail.com>
2025-03-23 21:38:01 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,15 @@
+import {createRoot} from 'react-dom/client'
+import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <div>Hello World</div>
+   },
+])
+const root = createRoot(document.querySelector('#root'))
+
+root.render(<RouterProvider router={router} />)
